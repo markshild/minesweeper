010c37d16445cd5f0d1bb11cbc5b013cd10b8bfc
Jake and Mark <jake+mark@appacademy.io>
2014-11-17 11:20:53 -0500

    Initialize Board and skeleton for Tile methods

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,12 +4,34 @@ module Minesweeper
     def initialize
 
     end
+
+    def bombed?
+
+    end
+
+    def flagged?
+
+    end
+
+    def revealed?
+
+    end
+
   end
 
   class Board
 
-    def initialize(size = 9)
-      
+    def initialize(size = 9, mines = 10)
+      @grid = Array.new(size) { Array.new(size) { Tile.new } }
+      mines.times do
+        loop do
+          x, y = rand(size -1), rand(size - 1)
+          unless @grid[x][y].bombed?
+            @grid[x][y].bomb
+            break
+          end
+        end
+      end
     end
 
   end
